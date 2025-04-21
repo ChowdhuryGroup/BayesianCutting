87660ac8731bc6f32a34c53dece8e084654bce2e
Liam Clink <clink.6@osu.edu>
2025-04-20 22:12:53 -0400

    Working on modifying the template as desired. still need to figure out hatch commands

--- beamConstruct.py
+++ beamConstruct.py
@@ -11,16 +11,18 @@ PORT = 11350
 BEAMSERVER_PATH = r'C:\Program Files\HALsoftware\Beamserver.exe'
 
 # Project settings
-base_project = r'C:\Users\twardowski.6a\Documents\GlassCutting\2025-04-17 messing With Beamserver\baseTemplate.beamp'
+base_project = r'C:\Users\twardowski.6a\Documents\GlassCutting\2025-04-17 messing With Beamserver\BayesianTemplate.beamp'
 output_template = 'circle_speed{speed}_spacing{spacing}_hatch{hatch}.beamp'
 
-# Sweep parameters
-speeds = [100, 200, 300]              # Pen marking speed
-spacings = [0.05]           # Hatch spacing
-hatch_counts = [1]              # Hatch repetitions
+#Trial Settings
+trial_number = "01"
+laser_speed = "3" #mm/s
+repeats = "5"
+hatches = "5"
+hatch_distance = ".005"
 
-# Element path to the hatch (adjust this as needed)
-element_path = 'element1.hatch'  # Replace with actual element path from BeamConstruct
+#Names in the beamproject of the elements
+elements = ["Circle 1","Circle 2","Circle 3","Circle 4","Rectangle 1","Rectangle 2","Rectangle 3", "Rectangle 4"]
 
 # Start BeamServer via CLI
 print("Starting BeamServer...")
@@ -28,7 +30,7 @@ print("Starting BeamServer...")
 #This is literally just addition of parameters, this is the dumbest way of flagging parameters into CLI i've ever seen
 subprocess.Popen([
     r"C:\Program Files\HALsoftware\BeamServer.exe",
-    "524290"
+    f"{524288+1+2+4+8}"
 ],
 cwd=r"C:\Program Files\HALsoftware"
 )
@@ -45,12 +47,33 @@ def send_cmd(cmd, s, expect_response = True):
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-
+    print(send_cmd("CmdGetElemNum",s))
+    time.sleep(2)
     print(send_cmd(f'CmdLoadPrj {base_project}', s))
-    print(send_cmd("CmdAddCircle",s))
+    print(send_cmd("CmdGetElemNum",s))
 
-    output_name = r'C:\Users\twardowski.6a\Documents\GlassCutting\2025-04-17 messing With Beamserver\test.beamp'
+    #Set Hatch distance, hatch count and repeats
+    for element in elements:
+        send_cmd(f"CmdSelEntName {element}",s)
+        send_cmd(f"CmdSetHatchDist {hatch_distance*1000}")
+    
+    #Set Pen speed
+    send_cmd(f"CmdSetPenMSpeed 0 {laser_speed*1000}",s) #This command takes speed in units of microns per second
+
+
+
+    #Set Label Mark Name
+    send_cmd("CmdSelEntName Label",s)
+    send_cmd(f"CmdSetElemText {trial_number}",s)
+
+
+    #Save new Trial
+    output_name = f'C:\\Users\\twardowski.6a\\Documents\\GlassCutting\\2025-04-17 messing With Beamserver\\test{trial_number}.beamp'
     send_cmd(f'CmdSavePrj {output_name}', s)
+
+    while True:
+        i = input("Enter command:")
+        print(send_cmd(f'{i}',s))
 
     # Exit UI
     print("\nSending EXITUI...")
